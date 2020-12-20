a7cc6ebaa882828a3308f0b68e8d7362fb06cc6f
MatsievichSasha <matsievich.sasha@gmail.com>
2020-12-20 13:33:41 +0200

    201220

---
+++ learn/useState.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function computeInitialCounter() {
+  console.log("some Calc");
+  return Math.trunc(Math.random() * 20);
+}
+
+function App() {
+  /*  const [counter, setCounter] = useState(0); */ //  можно вызывать сколько угодно раз
+  //const [counter, setCounter] = useState(computeInitialCounter()); приэтом способе функиция калс будет вызываться каждый раз при баттон
+  const [counter, setCounter] = useState(() => {
+    return computeInitialCounter();
+  });
+
+  const [state, setState] = useState({
+    title: "Count",
+    date: Date.now(),
+  });
+
+  /*  const counterState = useState(0); ///#1
+
+  console.log(counterState[0]); // состояние
+  console.log(counterState[1]); // функция меняющее состояние */
+
+  /*   if(true){
+    const counterState = useState(0) //внитури условия нельзя
+  } */
+
+  function increment() {
+    /* setCounter(counter + 1) */
+    //если нужно функцию выполнить два раза то в setCounter ложим коллбэк
+    setCounter((prev) => prev + 1);
+    setCounter((prev) => prev + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
+
+  function updateTitle() {
+    setState((prev) => {
+      return {
+        ...prev,
+        title: "New",
+      };
+    });
+  }
+
+  return (
+    <div>
+      <h1>Счетчик: {counter}</h1>
+      <button onClick={increment} className="btn btn-success">
+        Добавить
+      </button>
+      <button onClick={decrement} className="btn btn-success">
+        Убрать
+      </button>
+      <button onClick={updateTitle} className="">
+        Object
+      </button>
+      <pre>{JSON.stringify(state)}</pre>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+ const [type, setType] = useState('user')
+ 
+/* useEffect(()=>{
+  console.log('render')
+}) */
+
+useEffect(()=>{
+  console.log('type change', type)
+}, [type]) // useEffect принимает второй компонент который показыывает от чего должен зависеть useEffect будет вызываться только в том случаее если изменился type
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<h1>Ресурс: {type}</h1>
+<button onClick={()=>setType('users')}>Пользователи</button>
+<button onClick={()=>setType('Todo')}>Todo</button>
+<button onClick={()=>setType('Посты')}>Посты</button>
     </div>
   );
 }
