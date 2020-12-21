ab26700c2a0f86fa549548829b3569cd358f0a41
MatsievichSasha <matsievich.sasha@gmail.com>
2020-12-21 12:33:22 +0200

    useCallback

--- src/ItemsList.js
+++ src/ItemsList.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react"
 
-export default function ItemsList(getItems){
- 
+export default function ItemsList({getItems}) {
   const [items, setItems] = useState([])
- 
-  
+  //как только будет изменяться getItems мы будет заполнять наш массив
+  // будем следить за изменениями с помощью  useEffect
+  //все работает но если проследить а сколько раз вызывается 
+  //useEffect увидим что он вызывается не только при добавлении элемента
+  // но и при изменении цвета. Причина такая же как и при примере о useMemo
+  //поэтому используется hook useCallback который по сути делает тоже самое что и useMemo
+  useEffect(() => {
+    const newItems = getItems()
+    setItems(newItems)
+    console.log('render')
+  }, [getItems])
 
   return (
     <ul>
-      {}
+      {items.map((i) => (
+        <li key={i}>{i}</li>
+      ))}
     </ul>
   )
 }
